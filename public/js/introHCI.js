5eb1175cc9ff52c90b6e863c7472ff6e74ba67b8
yidie-ling <57084794+yidie-ling@users.noreply.github.com>
2020-02-20 02:58:17 -0800

    lab7

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -9,5 +9,12 @@ $(document).ready(function() {
  * Function that is called when the document is ready.
  */
 function initializePage() {
+	$(".likeBtn").click(clickLike);
+  $(".btn btn-default likeBtn").click(clickLike);
 	// your code here
+}
+
+function clickLike(){
+	console.log("Clicked");
+	ga("send", "event", "like", "click");
 }
